f6bd3cfd222e010a84058b55f6c71c662754d937
Kamil Dawid Grzebien <92851784+Kamigre@users.noreply.github.com>
2025-11-23 00:20:43 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,5 +5,11 @@ CONFIG = {
     "rl_policy": "MlpPolicy",
     "windows": [60],
     "rl_timesteps": 50000,
-    "half_life_max": 60
+    "half_life_max": 60,
+    "rl_lookback": 30,
+    "shock_prob": 0.01,
+    "shock_scale": 0.4,
+    "max_workers": 2,
+    "initial_capital": 10000,
+    "risk_free_rate": 0.4
 }
